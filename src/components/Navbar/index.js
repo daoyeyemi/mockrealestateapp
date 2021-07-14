453537fb3ed3d60cba2f93eb09361ab4f90e8546
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-07-14 14:03:23 -0700

    added some flavoring to Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const Navigation = styled.nav`
+    background: red;
+    height: 60px;
+    `;
+
+const Logo = styled(Link)`
+    color: #fff
+`
+
+const MenuBars = styled.i``;
+
+const NavigationMenu = styled.div``;
 
 export default function Navbar() {
     return (
-        <div>
-            Yoooooo
-        </div>
+        <Navigation>
+            <Logo>Living</Logo>
+            <MenuBars />
+            <NavigationMenu />
+        </Navigation>
     )
 }
