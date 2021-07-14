007d0da97bdfaa9da956a41bdcf70fd3f9e24266
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-07-14 15:38:55 -0700

    added media screen adjustments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from "./components/Navbar"
+import GlobalStyle from '../src/globalStyles';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <GlobalStyle/>
       <Navbar />
     </div>
   );
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,6 +3,7 @@ import styled,{css} from 'styled-components/macro'
 import { Link } from 'react-router-dom'
 import menuData from '../../data/dataMenu'
 import Button from '../Button';
+import { FaBars } from 'react-icons/fa'
 
 const Navigation = styled.nav`
     background: black;
@@ -15,6 +16,7 @@ const Navigation = styled.nav`
     width: 100%;
     background: red;
     `;
+
 const NavLink = css`
     color: #fff;
     display: flex;
@@ -28,19 +30,42 @@ const NavLink = css`
 const Logo = styled(Link)`
     ${NavLink}
     font-style: italic;
+    ;
 `
 
 const NavigationBtn = styled.div`
     display: flex;
     align-items: center;
     margin-right: 24px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
 `
-const MenuBars = styled.i``;
+const MenuBars = styled(FaBars)`
+    display: none;
+
+    @media screen and (max-width: 768px) {
+        display: block;
+        height: 40px;
+        width: 40px;
+        cursor: pointer;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-50%, 25%);
+        color: white;
+    }
+`;
 
 const NavigationMenu = styled.div`
-display: flex;
-align-items: center;
-margin-right: 48px;
+    display: flex;
+    align-items: center;
+    margin-right: -48px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
 `;
 
 const NavMenuLinks = styled(Link)`
